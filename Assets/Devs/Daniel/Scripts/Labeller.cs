aa630d39bdf82c00362fd224077dc50937650edb
BlackHawk <danielzegwaard@gmail.com>
2026-05-27 15:42:46 +0200

    Grid movement with mousclick

---
+++ Assets/Devs/Daniel/Scripts/Labeller.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+[ExecuteAlways]
+public class Labeller : MonoBehaviour
+{
+    TextMeshPro label;
+    public Vector2Int cords = new Vector2Int();
+    GridManager gridManager;
+
+    private void Awake()
+    {
+        gridManager = FindAnyObjectByType<GridManager>();
+        label = GetComponentInChildren<TextMeshPro>();
+
+        DisplayCords();
+    }
+
+    private void Update()
+    {
+        DisplayCords();
+        transform.name = cords.ToString();
+    }
+
+    private void DisplayCords()
+    {
+        if (!gridManager) { return; }
+        cords.x = Mathf.RoundToInt(transform.position.x / gridManager.UnityGridSize);
+        cords.y = Mathf.RoundToInt(transform.position.z / gridManager.UnityGridSize);
+
+        label.text = $"{cords.x}, {cords.y}";
+    }
+}
